c68214f7a4283d53c8bad1ea658be5e71bea3a5b
iscario <iscario@tlen.pl>
2018-07-23 15:22:54 +0200

    fixing issues

--- main.js
+++ main.js
@@ -1,38 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-
-// http.createServer(function (req, res){
-//   if (req.url == '/'){
-//     fs.readFile('./list.json', function(err, data) {
-//       if (err) {
-//         console.error(err);
-//         res.end('Błąd wczytania danych!');
-//       }
-//       else{
-//         var teksty = JSON.parse(data.toString());
-
-//         fs.readFile('./index.html', function(err, data){
-//           if (err) {
-//             console.error(err);
-//             res.end('Błąd wczytania układu strony!');
-//           }
-//           else {
-//             let templ = data.toString();
-
-//             let page = templ.replace('%', teksty.join('</li><li>'));
-//             res.writeHead(200, {'Content-Type': 'text/html'});
-//             res.end(page);
-//           }
-//         });
-//       }
-//     });
-//   }else{
-//     res.end('No Page!')
-//   }
-// }).listen(8080);
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function (req, res){
-  getTitles(res);
+  if (req.url == '/'){
+    getTitles(res);
+  } else {
+    res.end("No such Page!");
+  }
+
 }).listen(8080);
 
 function getTitles(res){
@@ -51,18 +26,19 @@ function getTemplate(titles, res){
     if (err){
       logError(err, res);
     } else{
-      formatHTML(titles, data.toString, res);
+      formatHTML(titles, data.toString(), res);
     }
-  })
+  });
 
-};
+}
 
 function formatHTML(titles, tmpl, res){
   var html = tmpl.replace('%', titles.join('</li><li>'));
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(html);
 }
+
 function logError(err, res){
   console.error(err);
   res.end('Wystąpił błąd!');
-};
+}
